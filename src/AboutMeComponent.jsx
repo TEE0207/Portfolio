40873663893b3113f34ec139a05440766bf14d93
Taofeek Lawal <taofeeklwl@gmail.com>
2025-04-08 13:14:21 +0100

    png updated

--- src/AboutMeComponent.jsx
+++ src/AboutMeComponent.jsx
@@ -2,7 +2,27 @@ const AboutMeComponent = () => {
 
 
     return(
-        <div>
+        <div className={Styles.allContentContainer}>
+
+            <div className={styles.iconContainer}>
+               
+            </div>
+
+            <div className={styles.aboutDescriptionImageContainer}>
+                    
+                    <div className={styles.aboutNdescriptionContainer}>
+                        <div className={styles.aboutContainer}>
+
+                        </div>
+                        <div className={styles.descriptionContainer}>
+
+                        </div>
+                    </div>
+
+                    <div className={styles.imageContainer}>
+
+                    </div>
+            </div>
         
         
         
