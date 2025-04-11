0d3a9e34b286798787988f326bd6ca3d12f32c5b
Taofeek Lawal <taofeeklwl@gmail.com>
2025-04-11 20:14:12 +0100

    skills section updated

--- src/Home/SkillComponent.jsx
+++ src/Home/SkillComponent.jsx
@@ -12,7 +12,7 @@ const SkillComponent  = () => {
 
                         <div className={styles.theTwoIconContainer}>
 
-                            <div className={styles.dropIconContainer}>
+                            <div className={styles.scrollIconContainer}>
                               <img src="./Scroll.png" alt="scroll" />
                             </div>
 
@@ -27,9 +27,69 @@ const SkillComponent  = () => {
                         </div>
 
                 </div>
+                
+
+                <div className={styles.skillsAndTextContainer}>
+                    <div className={styles.skillsContainer}>
+                        <img src="./Title.png" alt="scroll" />
+                    </div>
+
+                    <div className={styles.textContainer}>
+                        <p> I am striving to never stop learning and improving</p>
+
+                    </div>
+
+                </div>
+
+                <div className={styles.webdevelopmetContainer}>
+                    <img src="./Web.png" alt="" />
+
+                </div>
 
 
+                <div className={styles.allSkillsContainer}>
 
+                    <div className={styles.eachIconAndTextContainer}>
+                        <div className={styles.skillImageContainer}>
+                          <img src="./htmlIcon.png" alt="html" className={styles.skillImage} />
+                        </div>
+
+                        <h5 className={styles.htmlText}>HTML</h5>
+
+                    </div>
+
+                    <div className={styles.eachIconAndTextContainer}>
+
+                        <div className={styles.skillImageContainer}>
+                          <img src="./cssIcon.png" alt="css" className={styles.skillImage} />
+                        </div>
+
+
+                            <h5 className={styles.cssText}>CSS</h5>
+
+                    </div>
+
+                    <div className={styles.eachIconAndTextContainer}>
+
+                        <div className={styles.skillImageContainer}>
+                        <img src="./jsIcon.png" alt="js" className={styles.skillImage} />
+                     </div>
+
+                        <h5 className={styles.jsText}>JS</h5>
+
+                    </div>
+
+                    <div className={styles.eachIconAndTextContainer}>
+
+                     <div className={styles.skillImageContainer}>
+                        <img src="./reactIcon.png" alt="react" className={styles.skillImage} />
+                     </div>
+
+                         <h5 className={styles.reactText}>REACT</h5>
+
+                    </div>
+
+                </div>
 
 
         </section>
