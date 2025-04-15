9ee4504f49d6679eda6bfee68b1e6231e9c9ddec
Taofeek Lawal <taofeeklwl@gmail.com>
2025-04-15 22:17:06 +0100

    Project page updated

--- src/Project/ProjectComponent.jsx
+++ src/Project/ProjectComponent.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import styles from "./projectcomponent.module.css"
+
 
 const ProjectComponent = () => {
+
   return (
     <div className={styles.allContentContainer}>
+
+        <div className={styles.imageContainer}>
+            <img src="./projectImage.png" alt="Image" className={styles.image}/>
+            
+        </div>
+
+        <div className={styles.allOtherContentContainer}>
+
+            <div className={styles.headingContainer}>
+                <h2>Country Explorer with Dark Mode </h2>
+            </div>
+
+            <div className={styles.descriptionContainer}>
+                <p>A responsive React app that fetches and displays country data via REST API. Includes search, filtering, detail pages, and light/dark theme toggling.</p>
+
+            </div>
+
+            <div className={styles.linkContainer}>
+                <a href="#" className={styles.anchor}>View Project </a>
+
+            </div>
+
+            <div className={styles.stackAndContentContainer}>
+                {/* <p>STACK  :</p> */}
+                <p className={styles.cssColor}>CSS</p>
+                <p className={styles.jsColor}>JS</p>
+                <p className={styles.reactColor}>REACT</p>
+            </div>
+        </div>
 
 
 
--- src/Project/ProjectPage.jsx
+++ src/Project/ProjectPage.jsx
@@ -1,8 +1,25 @@
+import ProjectComponent from "./ProjectComponent"
+import styles from "./projectpage.module.css"
+
 const ProjectPage = () => {
 
     return(
-        <div>
-            <p>This Is my project file</p>
+        <div className={styles.allContentContainer}>
+
+            <div className={styles.projectAndLineImageContainer}>
+                <h1 className={styles.projectH}>Projects</h1>
+                <img src="./lineImage.png" alt="LineImage" />
+                <p className={styles.projectT}>I had the pleasure of working with these awesome projects</p>
+
+            </div>
+            
+            <div className={styles.allProjectContainer}>
+                <ProjectComponent />
+                <ProjectComponent />
+                <ProjectComponent />
+            </div>
+
+
         </div>
     )
 }
