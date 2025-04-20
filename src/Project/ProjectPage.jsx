34919462bd4479a67875154bbb82b3ddcf6ebe41
Taofeek Lawal <taofeeklwl@gmail.com>
2025-04-20 04:17:00 +0100

    image updated

--- src/Project/ProjectPage.jsx
+++ src/Project/ProjectPage.jsx
@@ -23,18 +23,20 @@ const ProjectPage = () => {
                  view = {{href : "https://rest-countries-gilt-chi.vercel.app/" , text : "View Project Here"}}
                 
                 />
-                {/* <ProjectComponent 
+                <ProjectComponent 
+                   image="/payApi.png"
                    title= "Modern Responsive PayAPI Website"
                    description="Developed using React, JavaScript, and CSS. Fully responsive across all devices, featuring a polished multi-page layout with accessible form validation."
                    view={{href : "https://pay-api-pi-ebon.vercel.app/" , text : "View Project Here"}}
                 
                 />
                 <ProjectComponent 
+                   image="/desktop.png"
                    title= "Responsive Clipboard Landing Page"
                    description="Crafted with React, CSS, and JavaScript. A sleek, responsive landing page that adapts seamlessly across devices with elegant layout and interactive UI elements."
                    view={{href : "https://desktop-app-hazel.vercel.app/" , text : "View Project Here"}}
                 
-                /> */}
+                />
             </div>
 
             <div className={styles.seeMoreButtonContainer}>
